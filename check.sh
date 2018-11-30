c0e65e261e68b071fc62e175351a04e3d8b0645e
sofiangrh <137006200@student.unsil.ac.id>
2018-11-30 19:07:52 +0700

    updated script

---
+++ check.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set extglob
+
+SOURCE_FILE=README.md
+
+FDROID_SETTINGS=$( \
+            grep -ni "https://f-droid.org/packages" ${SOURCE_FILE} \
+            | sed "s/.*https:\/\/f-droid.org\/packages\///g" \
+            | sed "s/)].*/;/g" \
+            )
+
+
+FAILED=0
+
+while IFS=';' read -ra TOK; do
+    for i in "${TOK[@]}"; do
+        echo -ne "Check '${i}' ... "
+
+        if [[ "${i}" =~ ^([[:alnum:]]|[\._-])+$ ]];
+        then
+            echo -e "\e[0;32m OK \e[0m"
+        else
+            echo -e "\e[0;31m INVALID \e[0m"
+            let "FAILED += 1"
+        fi
+
+    done
+done <<< "${FDROID_SETTINGS}"
+
+if [[ ${FAILED} -ge 1 ]];
+then
+    echo "Invalid Urls: ${FAILED}"
+    exit 1
+fi
+
